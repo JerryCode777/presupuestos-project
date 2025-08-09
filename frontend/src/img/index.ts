2378ff56452a58ff5d3613a665aa9f9f9091e812
brayan <bcarlosac20@gmail.com>
2025-08-08 22:22:48 -0500

    nuevas imagenes proyecto

--- frontend/src/img/index.ts
+++ frontend/src/img/index.ts
@@ -1,4 +1,7 @@
 import logoProyecto from "./logoProyecto.png";
 import logoGoogle from "./logoGoogle.png";
+import documentosImagen from "./documentosImagen.png";
+import proyectosImagen from "./proyectosImagen.png";
+import perfilImagen from "./perfilImagen.png";
 
-export { logoProyecto, logoGoogle };
+export { logoProyecto, logoGoogle, documentosImagen, proyectosImagen, perfilImagen};
